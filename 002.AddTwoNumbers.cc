edc3688396f605bd5116c9e69b15f8e69acfaaf0
saitama <806126488@qq.com>
2017-12-20 16:19:43 +0800

    002.AddTwoNumbers: 经过修改的解法，可以进行大数相加，不过比较繁琐，没有优化

--- 002.AddTwoNumbers.cc
+++ 002.AddTwoNumbers.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 using namespace std;
 
 struct ListNode {
@@ -32,6 +33,7 @@ public:
         cout << "sum2 is :" << sum << endl;
         tmp = sum;
         //下面也可以使用numtolist函数
+        //不过使用numtolist不对，还是运算不了大数相加
         do {
             cur = new ListNode(tmp%10);
             cur->next = NULL;
@@ -46,9 +48,54 @@ public:
     }
 };
 
+//经过修改的解法，可以进行大数相加，不过比较繁琐，没有优化
+class Solution2 {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int tmp, sum = 0, ix1 = 0, ix2 = 0;
+        ListNode *head = NULL, *tail = NULL;
+        ListNode *cur1 = l1, *cur2 = l2, *cur = NULL;
+        int up = 0; //这个换成carry比较好
+        do {
+            sum = 0;
+            if (cur1) {
+                sum += cur1->val;
+                cur1 = cur1->next;
+                ix1++;
+            }
+            if (cur2) {
+                sum += cur2->val;
+                cur2 = cur2->next;
+                ix2++;
+            }
+            sum += up;
+            if (sum >= 10) {
+                up = 1;
+            } else {
+                up = 0;
+            }
+            cur = new ListNode(sum%10);
+            cur->next = NULL;
+            if (head == NULL) {
+                head = tail = cur;
+            } else {
+                tail->next = cur;
+                tail = cur;
+            }
+        } while (cur1 || cur2);
+        if (up) {
+            cur = new ListNode(1);
+            tail->next = cur;
+            tail = cur;
+        }
+        return head;
+    }
+};
+
+/*
+因为参数是int，无法存储大数，所以不能使用这个，用下面的stringtolist替换
 ListNode* numtolist(int num)
 {
-    cout << "in numtolist: " << num << endl;
     ListNode *head = NULL, *tail = NULL, *cur = NULL;
     int tmp = num;
     //数字肯定是需要运行一次的,即使是0，所以用do-while好点
@@ -64,6 +111,25 @@ ListNode* numtolist(int num)
     } while (tmp /= 10);
     return head;
 }
+*/
+ListNode* stringtolist(string s)
+{
+    ListNode *head = NULL, *tail = NULL, *cur = NULL;
+    int ix = 0;
+    //数字肯定是需要运行一次的,即使是0，所以用do-while好点
+    do {
+        cur = new ListNode(s[ix]-48);
+        cur->next = NULL;
+        cout << "cur->val: " << cur->val << endl;
+        if (head == NULL) {
+            head = tail = cur;
+        } else {
+            cur->next = head;
+            head = cur;
+        }
+    } while (s[++ix]);
+    return head;
+}
 
 void print_list(ListNode *l)
 {
@@ -73,12 +139,17 @@ void print_list(ListNode *l)
     } while (tmp = tmp->next);
 }
 
-int main(void) 
+int main(void)
 {
-    Solution1 s;
-    ListNode *l1 = numtolist(456);
+    Solution2 s;
+    string s1, s2;
+    cout << "请输入s1:";
+    cin >> s1;
+    cout << "请输入s2:";
+    cin >> s2;
+    ListNode *l1 = stringtolist(s1);
     print_list(l1);
-    ListNode *l2 = numtolist(789);
+    ListNode *l2 = stringtolist(s2);
     print_list(l2);
     ListNode *l3 = s.addTwoNumbers(l1, l2);
     print_list(l3);
