4a68f929459ddd9538532923f6d1787041f3a145
saitama <806126488@qq.com>
2018-01-04 16:04:53 +0800

    003.LongestSubstringWithoutRepeatingCharacters.cc 网上的解法:Sliding Window

--- 003.LongestSubstringWithoutRepeatingCharacters.cc
+++ 003.LongestSubstringWithoutRepeatingCharacters.cc
@@ -3,12 +3,15 @@
 #include <unordered_set>
 using namespace std;
 
-class Solution {
+//最初始的解法，比较笨
+class Solution1 {
 public:
     int lengthOfLongestSubstring(string s) {
         string tmp, res;
         int maxsublen = 0;
         unordered_set<char> hs;
+        //可以稍微优化下，把s.length()赋值给n，否则每次比较
+        //都要调用length()函数
         for (int i = 0; i < s.length(); i++) {
             int j = i;
             do {
@@ -31,11 +34,41 @@ public:
     }       
 };
 
+class Solution2 {
+public:
+    int lengthOfLongestSubstring(string s) {
+        string::iterator i = s.begin(), j = s.begin();
+        string res;
+        unordered_set<char> hs;
+        int len = s.length();
+        int maxsublen = 0;
+        while (j < s.end()) {
+            if (hs.find(*j) != hs.end()) {
+                hs.erase(hs.find(*i));
+                i++;
+            } else {
+                hs.insert(*j);
+                j++;
+                if ((j - i) > maxsublen) {
+                    maxsublen = j - i;
+                    res.clear();
+                    res.assign(i, j);
+                }
+            }
+        }
+        cout << res << endl;
+    return res.length();
+    }
+};
+
 int main(void)
 {
-    string s = "pwwkew";
-    Solution s1;
-    int maxsublen = s1.lengthOfLongestSubstring(s);
+    string s;
+    Solution1 s1;
+    Solution2 s2;
+    cout << "请输入任意字符串：" << endl;
+    cin >> s;
+    int maxsublen = s2.lengthOfLongestSubstring(s);
     cout << maxsublen << endl;
     return(0);
 }
