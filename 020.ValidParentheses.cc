01a09505fa2f00a1aebc5a669b4281904a268aec
saitama <806126488@qq.com>
2018-08-07 20:53:22 +0800

    20.Valid Parentheses 之前的答案有错误，没有考虑到输入为[的情况，1h

--- 020.ValidParentheses.cc
+++ 020.ValidParentheses.cc
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <unordered_map>
 using namespace std;
 
 class solution {
 public:
     bool isvalid(string s) {
         stack<char> sc;
+        unordered_map<char, char> hm({{'}','{'}, {']', '['}, {')', '('}});
+
+        // 这个#用的很巧妙，不需要判断sc.empty()，否则直接sc.top()会段错误
+        sc.push('#');
         for (string::iterator iter = s.begin(); iter != s.end(); iter++) {
             if (*iter == '{' || *iter == '[' || *iter == '(') {
                 sc.push(*iter);
-            } else if ((*iter == '}' && sc.top() == '{')
-                    || (*iter == ']' && sc.top() == '[') 
-                    || (*iter == ')' && sc.top() == '(')) {
-                sc.pop();
+            } else if (*iter == '}' || *iter == ']' || *iter == ')') {
+                if (hm[*iter] == sc.top())
+                    sc.pop();
+                else
+                    return false;
             }
         }
-        return sc.empty() ? true : false;
+        return sc.top() == '#' ? true : false;
     }
 };
 
