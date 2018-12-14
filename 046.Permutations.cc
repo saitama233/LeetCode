99ff306fa52684d366385b3f50f572c9c90079a3
saitama <806126488@qq.com>
2018-12-14 21:01:22 +0800

    046.Permutations 3h，两次AC， Runtime: 12 ms, faster than 42.63% of C++ online submissions for Permutations.

---
+++ 046.Permutations.cc
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+ * 本来想用回溯了，不过感觉有些麻烦，而且一旦有重复的数字没想出来怎么算，
+ * 想起来之前做Next Permutation的时候在网上看过全排列可以用Next Permutation生成，就用这个了，
+ * Runtime: 12 ms, faster than 42.63% of C++ online submissions for Permutations.
+ * 发现答案里比较好的算法还是用的回溯，，
+ */
+class Solution1 {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>     vvec;
+        
+        sort(nums.begin(), nums.end());
+        vvec.push_back(nums);
+        if (!nums.size()) {
+            return vvec;
+        }
+        while (next_permutation(nums) != -1) {
+            vvec.push_back(nums);
+        }
+        
+        return vvec;
+    }
+    
+private:
+    int next_permutation(vector<int> &nums) {
+        vector<int>::iterator front;
+        vector<int>::iterator behind;
+        for (front = nums.end() - 1; front != nums.begin(); front--) {
+            if (*front > *(front - 1))
+                break;
+        }
+        if (front == nums.begin())
+            return -1;
+        
+        for (behind = nums.end() - 1; behind >= front; behind--) {
+            if (*behind > *(front - 1)) {
+                swap(behind, front - 1);
+                break;
+            }
+        }
+        sort(front, nums.end());
+        return 0;
+    }
+    
+    void swap(vector<int>::iterator a, vector<int>::iterator b) {
+        *a ^= *b;
+        *b ^= *a;
+        *a ^= *b;
+    }
+};
+
+void print(vector<int> &vec)
+{
+    vector<int>::iterator iter;
+    for (iter = vec.begin(); iter != vec.end(); iter++) {
+        cout << *iter;
+        cout << (iter + 1 == vec.end() ? '\n' : ',');
+    }
+}
+
+int main()
+{
+    Solution1 s;
+    vector<vector<int>> vvec;
+    vector<vector<int>> ret;
+    vvec.push_back({});
+    vvec.push_back({1,2,3,4});
+    vvec.push_back({1,1,2});
+    
+    vector<vector<int>>::iterator viter;
+    vector<vector<int>>::iterator riter;
+    vector<int>::iterator iter;
+    for (viter = vvec.begin(); viter != vvec.end(); viter++) {
+        cout << "The vector: ";
+        print(*viter);
+        ret = s.permute(*viter);
+        cout << "The result is:\n";
+        cout << "{" << endl;
+        for (riter = ret.begin(); riter != ret.end(); riter++) {
+            cout << "\t{";
+            for (iter = (*riter).begin(); iter != (*riter).end(); iter++) {
+                cout << *iter;
+                cout << (iter + 1 == (*riter).end() ? "" : ",");
+            }
+            cout << "}" << endl;
+        }
+        cout << "}\n" << endl;
+    }
+    
+    return 0;
+}
